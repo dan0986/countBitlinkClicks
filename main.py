aa8410deb9d84f0468232be9da97a424534994a7
Dan Nikitin <lfybkf86@mail.ru>
2019-10-02 14:24:54 +0300

    Replace envs vars in Ifmain block
    Replace envs vars in Ifmain block.

--- main.py
+++ main.py
@@ -3,19 +3,13 @@ import argparse
 import requests
 from dotenv import load_dotenv
 
-BITLY_TOKEN = os.getenv("BITLY_TOKEN")
-BITLY_BASE_URL = "https://api-ssl.bitly.com/v4/{}"
-HEADERS = {
-    "Authorization": BITLY_TOKEN
-    }
-
 def get_shorten_link(token, url):
     """ Return shorten link from url via bit.ly"""
     payload = {
       "long_url": url,
       "title": "create shorten_link"
     }
-    response = requests.post(BITLY_BASE_URL.format("bitlinks"), headers=HEADERS, json=payload)
+    response = requests.post(bitly_base_url.format("bitlinks"), headers=headers, json=payload)
     response.raise_for_status()
     shorten_link = response.json()["id"]
     return shorten_link
@@ -26,8 +20,8 @@ def get_count_clicks(token, link):
       "units": "-1",
       "unit": "day"
     }
-    url = BITLY_BASE_URL.format("bitlinks") + "/{}/clicks/summary".format(link)
-    response = requests.get(url, headers=HEADERS, params=payload)
+    url = bitly_base_url.format("bitlinks") + "/{}/clicks/summary".format(link)
+    response = requests.get(url, headers=headers, params=payload)
     response.raise_for_status()
     count_clicks = response.json()["total_clicks"]
     return count_clicks
@@ -40,17 +34,22 @@ def main():
     args = parser.parse_args()
     if args.url.startswith("bit.ly"):
         try:
-            print("По Вашей ссылке перешли ", get_count_clicks(BITLY_TOKEN, args.url), " раз(а)")
+            print("По Вашей ссылке перешли ", get_count_clicks(bitly_token, args.url), " раз(а)")
         except requests.exceptions.HTTPError:
             print("Упс!!! Что-то пошло не так.")
     else:
         try:
-            bitlink = get_shorten_link(BITLY_TOKEN, args.url)
+            bitlink = get_shorten_link(bitly_token, args.url)
             print("Короткая ссылка", bitlink)
-            print("По вашей ссылке перешли ", get_count_clicks(BITLY_TOKEN, bitlink), " раз(а)")
+            print("По вашей ссылке перешли ", get_count_clicks(bitly_token, bitlink), " раз(а)")
         except requests.exceptions.HTTPError:
             print("Упс!!! Вы ввели неверную ссылку. Проверьте ее и попробуйте еще раз...")
 
 if __name__ == "__main__":
     load_dotenv()
+    bitly_token = os.getenv("BITLY_TOKEN")
+    bitly_base_url = "https://api-ssl.bitly.com/v4/{}"
+    headers = {
+        "Authorization": bitly_token
+    }
     main()
